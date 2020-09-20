a6ead6a4aa97391597a9e45d67802e7b04e46a63
vitorcm2 <vitorcm2@al.insper.edu.br>
2020-09-20 19:51:14 -0300

    Acertos 200

--- test_main.py
+++ test_main.py
@@ -4,7 +4,7 @@ import uuid
 
 client = TestClient(app)
 
-# ERROS 404
+# TESTES ERROS 404
 
 
 def test_read_main_returns_not_found():
@@ -40,6 +40,7 @@ def test_patch_unexistent_task():
         f"/task/{uuid_}", json={"description": "testing error", "completed": "False"})
     assert response.status_code == 404
     assert response.json() == {'detail': 'Task not found'}
+    
 
 # ERROS 422
 
@@ -67,7 +68,7 @@ def test_put_unvalid_task():
     assert response.json() == {'detail': [{'loc': [
         'path', 'uuid_'], 'msg': 'value is not a valid uuid', 'type': 'type_error.uuid'}]}
 
-# ACERTOS 200
+# TESTES ACERTOS 200
 
 
 def test_get_list_existent_task():
@@ -75,10 +76,73 @@ def test_get_list_existent_task():
     assert response.status_code == 200
     # json da lista sempre vazia no começo
     assert response.json() == {}
-
-
-def test_get_existent_task():
-    # json para existir uma task
+    
+def test_get_completed_task_list():
+    response_true = client.post(
+        "/task", json={"description": "testing error", "completed": "True"})
+    response_false = client.post(
+        "/task", json={"description": "testing error", "completed": "False"})
+    response = client.get('/task?completed=true')
+    assert response.status_code == 200
+    # json da lista sempre vazia no começo
+    assert response.json() == {response_true.json():{"description": "testing error", "completed": True}}
+    responsedelete1 = client.delete(f"/task/{response_true.json()}")
+    responsedelete1 = client.delete(f"/task/{response_false.json()}")
+    
+def test_get_incompleted_task_list():
+    response_true = client.post(
+        "/task", json={"description": "testing error", "completed": "True"})
+    response_false = client.post(
+        "/task", json={"description": "testing error", "completed": "False"})
+    response = client.get('/task?completed=false')
+    assert response.status_code == 200
+    # json da lista sempre vazia no começo
+    assert response.json() == {response_false.json():{"description": "testing error", "completed": False}}
+    responsedelete1 = client.delete(f"/task/{response_true.json()}")
+    responsedelete1 = client.delete(f"/task/{response_false.json()}")
+    
+def test_create_a_new_task():
     response = client.post(
         "/task", json={"description": "testing error", "completed": "False"})
     assert response.status_code == 200
+    response1 = client.delete(f"/task/{response.json()}")
+    
+def test_get_existent_task():
+    response = client.post(
+        "/task", json={"description": "testing error", "completed": "False"})
+    #assert response.status_code == 200
+
+    response1 = client.get(f"/task/{response.json()}")
+    assert response1.status_code == 200
+    response1 = client.delete(f"/task/{response.json()}")
+    
+    
+def test_put_task():
+    response = client.post(
+        "/task", json={"description": "testing error", "completed": "False"})
+    #assert response.status_code == 200
+
+    response1 = client.put(f"/task/{response.json()}",json={"description": "testing error", "completed": "False"})
+    assert response1.status_code == 200
+    response1 = client.delete(f"/task/{response.json()}")
+
+def test_delete_existent_task():
+    response = client.post(
+        "/task", json={"description": "testing error", "completed": "False"})
+    #assert response.status_code == 200
+
+    response1 = client.delete(f"/task/{response.json()}")
+    assert response1.status_code == 200
+    
+def test_patch_task():
+    response = client.post(
+        "/task", json={"description": "testing error", "completed": "False"})
+    #assert response.status_code == 200
+
+    response1 = client.patch(f"/task/{response.json()}",json={"description": "testing error", "completed": "False"})
+    assert response1.status_code == 200
+    response1 = client.delete(f"/task/{response.json()}")
+    
+
+    
+    
